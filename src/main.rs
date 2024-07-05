b2f2dd858806eeac660f754066601ccea6fd7bf0
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-05 10:07:50 +0900

    함수의 인자 전달

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,10 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(5);
 }
 
 // 러스트에서의 함수는 아무 위치에나 정의할 수 있음.
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
 }
