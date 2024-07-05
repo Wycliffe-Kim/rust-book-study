e1da99da29ef0e2164d5fa98ce4930f12bae6840
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-05 15:41:21 +0900

    dangling 참조를 해결하려면 차라리 값을 반환해야 함

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,12 @@
 
 fn main() {
     let reference_to_nothing = dangle();
+
+    println!("{}", reference_to_nothing);
 }
 
-fn dangle() -> &String {
-    // dangle returns a reference to a String
+fn dangle() -> String {
+    let s = String::from("hello");
 
-    let s = String::from("hello"); // s is a new String
-
-    &s // we return a reference to the String, s
-} // Here, s goes out of scope, and is dropped. Its memory goes away.
-  // Danger!
+    s
+}
