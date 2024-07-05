6519108859a5172918527f5774eee04c156d822c
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-05 10:07:17 +0900

    함수는 아무 위치에서나 선언할 수 있음

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2024 nota inc. All rights reserved.
+ * This source code is the property of nota inc. and is protected by copyright law. THE RECEIPT OR POSSESSION OF  THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY TRANSFER OF OWNERSHIP, COPYRIGHT OR ANY RIGHTS INCLUDING BUT NOT LIMITED TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+ * Unauthorized disclosure, reproduction, modification, distribution, or commercial use of all or any part of this source code without prior written consent from Nota Inc. is strictly prohibited.
+ * This source code is provided "as is" and without any warranty, express or implied.
+ * Nota Inc. shall not be liable for any damages arising from the use of this source code.
+ * For inquiries regarding the use of this source code, please contact Nota Inc. at:
+ * Email: contact@nota.ai
+ */
+
 fn main() {
     println!("Hello, world!");
+
+    another_function();
+}
+
+// 러스트에서의 함수는 아무 위치에나 정의할 수 있음.
+fn another_function() {
+    println!("Another function.");
 }
