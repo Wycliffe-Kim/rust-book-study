e2db67973539e39d9f126e25406729a3e8214437
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-05 15:40:37 +0900

    함수가 함수 내부에서 사용하는 참조를 반환할 수 없음. 참조는 그 해당 함수에서만 유효하기 때문

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,14 @@
  */
 
 fn main() {
-    let mut s = String::from("hello");
-
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{r1} and {r2}");
-    // variables r1 and r2 will not be used after this point
-
-    let r3 = &mut s; // no problem
-    println!("{r3}");
+    let reference_to_nothing = dangle();
 }
+
+fn dangle() -> &String {
+    // dangle returns a reference to a String
+
+    let s = String::from("hello"); // s is a new String
+
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope, and is dropped. Its memory goes away.
+  // Danger!
