e39d06c7aafb5d7a14743540a71b2a8c3087a99d
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-05 10:34:23 +0900

    for 루프 사용하면 범위 순회가 가능

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,9 @@
  */
 
 fn main() {
-    let a = [10, 20, 30, 40, 50];
-    let mut index = 0;
-
-    println!("----- while loop -----");
-    while index < 5 {
-        println!("the value is: {}", a[index]);
-
-        index += 1;
+    // reversing order
+    for number in (1..4).rev() {
+        println!("{number}!");
     }
-
-    println!("----- for loop -----");
-    for element in a {
-        println!("the value is: {element}");
-    }
+    println!("LIFTOFF!!!");
 }
