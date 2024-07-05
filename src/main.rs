2cf4dea57b6479ccd190f7427d07e1f4970c2ef4
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-05 15:36:55 +0900

    참조를 했던 값들이 사용되고 나서는 mutable 참조를 할 수 있다

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,13 @@
  */
 
 fn main() {
-    let mut s1 = String::from("hello");
-    println!("The length of '{s1}' before changing is {}.", s1.len());
+    let mut s = String::from("hello");
 
-    let r1 = &s1;
-    let r2 = &s1;
-    let r3 = &mut s1;
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{r1} and {r2}");
+    // variables r1 and r2 will not be used after this point
 
-    println!("{}, {}, {}", r1, r2, r3);
-}
-
-fn calculate_length(s: &mut String) -> usize {
-    s.push_str("world");
-    s.len()
+    let r3 = &mut s; // no problem
+    println!("{r3}");
 }
