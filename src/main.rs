66d070fe003c6873075580da60fbeef6dca8c6ea
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-06 22:15:50 +0900

    ? 오퍼레이터는 반환형이 Result, Option, FromResidual일 때만 가능

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,7 @@
  */
 
 use std::fs::File;
-use std::io::{self, Read};
-
-fn read_username_from_file() -> Result<String, io::Error> {
-    let mut username = String::new();
-
-    File::open("hello.txt")?.read_to_string(&mut username)?;
-
-    Ok(username)
-}
 
 fn main() {
-    match read_username_from_file() {
-        Ok(username) => println!("Username: {}", username),
-        Err(e) => println!("Error: {}", e),
-    }
+    let greeting_file = File::open("hello.txt")?;
 }
