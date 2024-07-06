58439786a1565c507bb2253496f8c6f1a0a9df97
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-06 17:18:08 +0900

    hash map에는 참조도 넣을 수 있기는 함...

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,17 @@
 
 use std::collections::HashMap;
 
+fn insert(key: &String, value: &String) {
+    let mut map = HashMap::new();
+    map.insert(key, value);
+
+    println!("{key} = {value}");
+}
+
 fn main() {
     let field_name = String::from("Favorite color");
     let field_value = String::from("Blue");
-
-    let mut map = HashMap::new();
-    map.insert(field_name, field_value);
-    // field_name and field_value are invalid at this point, try using them and
-    // see what compiler error you get!
+    insert(&field_name, &field_value);
 
     println!("{field_name} = {field_value}");
 }
