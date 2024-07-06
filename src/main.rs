8f716d6d7951a305a473dd7e1669935b2bb592f6
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-06 22:38:02 +0900

    일반적인 제네릭 없는 예제

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2024 nota inc. All rights reserved.
+ * This source code is the property of nota inc. and is protected by copyright law. THE RECEIPT OR POSSESSION OF  THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY TRANSFER OF OWNERSHIP, COPYRIGHT OR ANY RIGHTS INCLUDING BUT NOT LIMITED TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+ * Unauthorized disclosure, reproduction, modification, distribution, or commercial use of all or any part of this source code without prior written consent from Nota Inc. is strictly prohibited.
+ * This source code is provided "as is" and without any warranty, express or implied.
+ * Nota Inc. shall not be liable for any damages arising from the use of this source code.
+ * For inquiries regarding the use of this source code, please contact Nota Inc. at:
+ * Email: contact@nota.ai
+ */
+
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let mut largest = &number_list[0];
+
+    for number in &number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {largest}");
 }
