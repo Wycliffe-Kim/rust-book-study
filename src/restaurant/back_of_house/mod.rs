40fb4eca6fc1e9ba60e716a954e4e28561833df8
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-06 14:41:20 +0900

    enum을 사용

--- src/restaurant/back_of_house/mod.rs
+++ src/restaurant/back_of_house/mod.rs
@@ -7,17 +7,5 @@
  * For inquiries regarding the use of this source code, please contact Nota Inc. at:
  * Email: contact@nota.ai
  */
-#[derive(Debug)]
-pub struct Breakfast {
-    pub toast: String,
-    seasonal_fruit: String,
-}
-
-impl Breakfast {
-    pub fn summer(toast: &str) -> Breakfast {
-        Breakfast {
-            toast: String::from(toast),
-            seasonal_fruit: String::from("peaches"),
-        }
-    }
-}
+pub mod appetizer;
+pub mod breakfast;
--- src/restaurant/mod.rs
+++ src/restaurant/mod.rs
@@ -10,15 +10,20 @@
 pub mod back_of_house;
 pub mod front_of_house;
 
-pub fn eat_at_restaurant() {
+fn front_of_house() {
+    println!("----- front_of_house -----");
     // Absolute path
     crate::restaurant::front_of_house::hosting::add_to_waitlist(1);
 
     // Relative path
     front_of_house::hosting::add_to_waitlist(2);
+}
+
+fn back_of_house() {
+    println!("----- back_of_house -----");
 
     // Order a breakfast in the summer with Rye toast
-    let mut meal = back_of_house::Breakfast::summer("Rye");
+    let mut meal = back_of_house::breakfast::Breakfast::summer("Rye");
     // Change our mind about what bread we'd like
     meal.toast = String::from("Wheat");
     println!("I'd like {} toast please", meal.toast);
@@ -26,4 +31,14 @@ pub fn eat_at_restaurant() {
     // The next line won't compile if we uncomment it; we're not allowed
     // to see or modify the seasonal fruit that comes with the meal
     // meal.seasonal_fruit = String::from("blueberries");
+    let order1 = back_of_house::appetizer::Appetizer::Soup;
+    let order2 = back_of_house::appetizer::Appetizer::Salad;
+
+    println!("{:?}", order1);
+    println!("{:?}", order2);
+}
+
+pub fn eat_at_restaurant() {
+    front_of_house();
+    back_of_house();
 }
