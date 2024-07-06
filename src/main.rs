811ff987f577214d13dcb2dc9e0fb02bb7608094
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-06 22:45:16 +0900

    제네릭 구조체 선언

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,15 @@
  * Email: contact@nota.ai
  */
 
-fn largest<T>(list: &[T]) -> &T {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
+struct Point<T> {
+    x: T,
+    y: T,
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
 
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {result}");
+    println!("integer.x = {}, integer.y = {}", integer.x, integer.y);
+    println!("float.x = {}, float.y = {}", float.x, float.y);
 }
