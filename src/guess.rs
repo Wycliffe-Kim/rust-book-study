43ff8b93e9accc67cc60515bfe9c71cda31df4de
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-08 15:24:34 +0900

    add should panic not passed

--- src/guess.rs
+++ src/guess.rs
@@ -8,14 +8,33 @@
  * Email: contact@nota.ai
  */
 
-pub mod adder;
-pub mod greeting;
-pub mod guess;
-pub mod rectangle;
+pub struct Guess {
+    value: i32,
+}
 
-fn main() {
-    println!("Hello, world!");
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}.");
+        }
 
-    let rect = rectangle::Rectangle::new(30, 50);
-    println!("rect is {:?}", rect);
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_50() {
+        Guess::new(50);
+    }
 }
