8c2dc4ccaa48d3e5c41c93d036b5e45a18f0cbba
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-08 12:33:22 +0900

    수명 규칙 readme

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,14 @@
  * Email: contact@nota.ai
  */
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
 
 fn main() {
-    let result;
-    let string1 = String::from("long");
-    {
-        let string2 = String::from("long1dsfafasfs"); // 이 경우는 힙에 할당되어서 result에서 확인할 수 없음.
-
-        result = longest(string1.as_str(), string2.as_str());
-    }
-    println!("The longest string is {}", result);
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
