b145ed3424a17a1e4ba156e4efe997b54e1b20ab
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-08 14:43:59 +0900

    첫 번째 테스트 작성

--- src/adder.rs
+++ src/adder.rs
@@ -8,8 +8,17 @@
  * Email: contact@nota.ai
  */
 
-pub mod adder;
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
 
-fn main() {
-    println!("Hello, world!");
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
 }
