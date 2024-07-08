8c7a8ef55b978e9bd6823f1fb80657144e8d866e
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-08 12:39:45 +0900

    수명이 생략되는 경우

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,16 @@ struct ImportantExcerpt<'a> {
     part: &'a str,
 }
 
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {announcement}");
+        self.part
+    }
+}
+
 fn main() {
     let novel = String::from("Call me Ishmael. Some years ago...");
     let first_sentence = novel.split('.').next().expect("Could not find a '.'");
