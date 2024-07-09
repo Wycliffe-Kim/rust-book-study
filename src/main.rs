05bbdfbc32e8eb69903b3b96532bfb6f8ee15805
Wycliffe-Kim <f16o@hanyang.ac.kr>
2024-07-09 14:37:16 +0900

    역참조 연산

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,28 @@
  * Email: contact@nota.ai
  */
 
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
 fn main() {
-    let list = List::Cons(
-        1,
-        Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))),
-    );
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
